37f08db762f8892996cb69c523c051a31a61a54b
A1phaBrav0 <a1phabrav0@A1phaBrav0.local>
2025-08-22 14:22:35 +0100

    complete 1.7 unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,11 +2,11 @@ import {useState} from 'react'
 
 const Button = ({text, onClick}) => <button onClick={onClick}>{text}</button>
 
-
 const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    let total = good + neutral + bad
 
     return (
         <div>
@@ -18,6 +18,9 @@ const App = () => {
             <div>good {good}</div>
             <div>neutral {neutral}</div>
             <div>bad {bad}</div>
+            <div>all {total}</div>
+            <div>average {total > 0 ? (good - bad) / total : 0}</div>
+            <div>positive {good > 0 ? ((good / total) * 100).toFixed(13) : 0} %</div>
         </div>
     )
 }
