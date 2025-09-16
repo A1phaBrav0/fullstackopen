336b0836f653849a35b6c7deedf1ab212e8c6560
A1phaBrav0 <184128778+A1phaBrav0@users.noreply.github.com>
2025-09-16 09:56:07 +0100

    complete 3.5 phonebook backend step 5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,4 @@
 const express = require("express")
-const {response} = require("express");
 const app = express()
 app.use(express.json())
 
@@ -26,13 +25,18 @@ let contacts = [
     }
 ]
 
+const generateId = () => {
+    const id = Math.random() * (900000 - 5) + 5
+    return String(Math.round(id))
+}
+
 app.get("/api/persons", (request, response) => {
     response.json(contacts)
 })
 
 app.get("/api/info", (request, response) => {
     response.send(
-`
+        `
         <div>
             <p>Phonebook has info for ${contacts.length} people</p>
             <p>${new Date().toString()}</p>
@@ -45,9 +49,9 @@ app.get("/api/persons/:id", (request, response) => {
     const id = request.params.id
     const person = contacts.find(p => p.id === id)
 
-   if (person) {
-       response.json(person)
-   } else response.send(404).end()
+    if (person) {
+        response.json(person)
+    } else response.send(404).end()
 
 })
 
@@ -55,6 +59,25 @@ app.delete("/api/persons/:id", (request, response) => {
     const id = request.params.id
     contacts = contacts.filter(p => p.id !== id)
     response.status(204).end()
+})
+
+app.post("/api/persons", (request, response) => {
+    const body = request.body
+
+    if (!body) {
+        return response.status(404).json({
+            error: "content missing"
+        })
+    }
+
+    const contact = {
+        id: generateId(),
+        name: body.name,
+        number: body.number,
+    }
+
+    contacts = contacts.concat(contact)
+    response.json(contact)
 })
 
 const PORT = 3001
